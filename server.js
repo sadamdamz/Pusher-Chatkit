3d2229b849ada0a65dea03546b668b8f9c7f9966
sadamdamz <sadamdamzzz@gmail.com>
2020-02-22 16:35:36 +0530

    Chatkit App

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
+
+const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:64137134-720e-4f83-a039-7a78b3e1e60a',
+  key:
+    'c7ebdfb4-4dba-4497-8864-8d6fb7937df0:hhws1vYhP+QhchMwCVSNQw68lSgxSmkT5YHE8baJE3k='
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.post('/authenticate', (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id })
+  res.status(authData.status).send(authData.body)
+})
+
+const PORT = 3001
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(`Running on port ${PORT}`)
+  }
+})
